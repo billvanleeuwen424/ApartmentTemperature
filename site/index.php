cd9bbf3c7e3b7c37129b05836690030580042d54
Bill <bill@pop-os.localdomain>
2021-09-19 19:00:49 -0400

    Big update. Forgot to push for a while :$

--- site/index.php
+++ site/index.php
@@ -26,17 +26,16 @@ $outsideHumidity = $outsideData[1];
     <title>Raspberry Pi Temperature</title>
 </head>
 <body>
+    <header>
+        <h1>My Room vs Outdoor Temperature</h1>
+        <h3>A Project by Bill Van Leeuwen</h3>
+    </header>
     <main>
         <div class="gridCont">
             <div class="pure-g">
-                <div class="pure-u-1">
-                    <p>Time of Data:</p>
-                </div>
-            </div>
-            <div class="pure-g">
                 <div class="pure-u-1 pure-u-md-1-2">
                     <div class="l-box">
-                        <span class="boxTitle">Room Climate</span>
+                        <span class="boxTitle">Room Climate (Raspberry Pi)</span>
                         <div class='pure-g'>
                             <div class='pure-u-2'>
                                 <div class="pure-g">
@@ -61,7 +60,7 @@ $outsideHumidity = $outsideData[1];
                 </div>
                 <div class="pure-u-1 pure-u-md-1-2">
                     <div class="l-box">
-                        <span class="boxTitle">Outer Climate</span>
+                        <span class="boxTitle">Outdoor Climate (API)</span>
                         <div class="pure-g">
                             <div class='pure-u-2'>
                                 <div class="pure-g">
@@ -82,6 +81,13 @@ $outsideHumidity = $outsideData[1];
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pure-g">
+                <div class="l-box">
+                    <div class="googleMap">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d81382.31549935584!2d-78.34904935233745!3d44.317301941907786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1629498728316!5m2!1sen!2sca" style="border:0;" frameborder=0></iframe>
                     </div>
                 </div>
             </div>
